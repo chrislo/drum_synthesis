6e5cc4a4fe63399d0a009010db8836783ba8beea
Chris Lowis <chris.lowis@gmail.com>
2015-03-23 15:40:39 +0000

    Simple play/pause button
    Play the drum loop using two buttons in the page.

---
+++ javascripts/drums.js
@@ -0,0 +1,139 @@
+function Kick(context) {
+  this.context = context;
+};
+
+Kick.prototype.setup = function() {
+  this.osc = this.context.createOscillator();
+  this.gain = this.context.createGain();
+  this.osc.connect(this.gain);
+  this.gain.connect(this.context.destination)
+};
+
+Kick.prototype.trigger = function(time) {
+  this.setup();
+
+  this.osc.frequency.setValueAtTime(150, time);
+  this.gain.gain.setValueAtTime(1, time);
+
+  this.osc.frequency.exponentialRampToValueAtTime(0.01, time + 0.5);
+  this.gain.gain.exponentialRampToValueAtTime(0.01, time + 0.5);
+
+  this.osc.start(time);
+
+  this.osc.stop(time + 0.5);
+};
+
+function Snare(context) {
+  this.context = context;
+};
+
+Snare.prototype.setup = function() {
+  this.noise = this.context.createBufferSource();
+  this.noise.buffer = this.noiseBuffer();
+
+  var noiseFilter = this.context.createBiquadFilter();
+  noiseFilter.type = 'highpass';
+  noiseFilter.frequency.value = 1000;
+  this.noise.connect(noiseFilter);
+
+  this.noiseEnvelope = this.context.createGain();
+  noiseFilter.connect(this.noiseEnvelope);
+
+  this.noiseEnvelope.connect(this.context.destination);
+
+  this.osc = this.context.createOscillator();
+  this.osc.type = 'triangle';
+
+  this.oscEnvelope = this.context.createGain();
+  this.osc.connect(this.oscEnvelope);
+  this.oscEnvelope.connect(this.context.destination);
+};
+
+Snare.prototype.noiseBuffer = function() {
+  var bufferSize = this.context.sampleRate;
+  var buffer = this.context.createBuffer(1, bufferSize, this.context.sampleRate);
+  var output = buffer.getChannelData(0);
+
+  for (var i = 0; i < bufferSize; i++) {
+    output[i] = Math.random() * 2 - 1;
+  }
+
+  return buffer;
+};
+
+Snare.prototype.trigger = function(time) {
+  this.setup();
+
+  this.noiseEnvelope.gain.setValueAtTime(1, time);
+  this.noiseEnvelope.gain.exponentialRampToValueAtTime(0.01, time + 0.2);
+  this.noise.start(time)
+
+  this.osc.frequency.setValueAtTime(100, time);
+  this.oscEnvelope.gain.setValueAtTime(0.7, time);
+  this.oscEnvelope.gain.exponentialRampToValueAtTime(0.01, time + 0.1);
+  this.osc.start(time)
+
+  this.osc.stop(time + 0.2);
+  this.noise.stop(time + 0.2);
+};
+
+function HiHat(context, buffer) {
+  this.context = context;
+  this.buffer = buffer;
+};
+
+HiHat.prototype.setup = function() {
+  this.source = this.context.createBufferSource();
+  this.source.buffer = this.buffer;
+  this.source.connect(this.context.destination);
+};
+
+HiHat.prototype.trigger = function(time) {
+  this.setup();
+
+  this.source.start(time);
+};
+
+var sampleLoader = function(url, context, callback) {
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
+
+  request.onload = function() {
+    context.decodeAudioData(request.response, function(buffer) {
+      callback(buffer);
+    });
+  };
+
+  request.send();
+};
+
+var context = new AudioContext();
+
+var play = function(buffer) {
+  var kick  = new Kick(context);
+  var snare = new Snare(context);
+  var hihat = new HiHat(context, buffer);
+
+  Tone.Transport.bpm.value = 120;
+
+  Tone.Transport.setInterval(function(time){ kick.trigger(time) }, "4n");
+  Tone.Transport.setInterval(function(time){ snare.trigger(time) }, "2n");
+  Tone.Transport.setInterval(function(time){ hihat.trigger(time) }, "8t");
+
+  Tone.Transport.start();
+};
+
+var enablePlay = function(buffer) {
+  $("#play").removeClass('pure-button-disabled');
+
+  $("#play").click(function() {
+    play(buffer);
+  });
+};
+
+$("#pause").click(function() {
+  Tone.Transport.stop();
+});
+
+sampleLoader('/samples/hihat.wav', context, enablePlay);
